a52fbed809b18c735bc0f07f9a32e0bf7e25cdc3
Nayjest <mail@vitaliy.in>
2025-09-09 16:43:31 +0200

    add missing files

---
+++ snowflake_mcp/config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Config:
+    account: str = field(default='')
+    token: str = field(default='')
+    database: str = field(default='')
+    schema: str = field(default='')
+    agent: str = field(default='')
+    defaults: "Config" = field(default=None)
+
+    def __post_init__(self):
+        if self.defaults:
+            for key, value in self.defaults.__dict__.items():
+                if not getattr(self, key):
+                    setattr(self, key, value)
